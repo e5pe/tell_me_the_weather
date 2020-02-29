702266eac8e9382cd2fff8ebc37903fe132c2b61
Esperanza Cutillas Rastoll <especr101@gmail.com>
2020-02-29 18:22:55 +0100

    Cleaning example json data.

--- web/views.py
+++ web/views.py
@@ -21,10 +21,7 @@ def index(request):
             units = getTemperature(temperatureUnits.lower())
             url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units={}&appid={}'
             city = form.cleaned_data['search']
-            # Para no gastar tantas llamadas a la api y arreglar la vista lo dejamos comentado
             r = requests.get(url.format(city, units, API_KEY)).json()
-            # r = {'coord': {'lon': -0.48, 'lat': 38.35}, 'weather': [{'id': 802, 'main': 'Clouds', 'description': 'scattered clouds', 'icon': '03d'}], 'base': 'stations', 'main': {'temp': 20.18, 'feels_like': 16.19, 'temp_min': 18.89, 'temp_max': 22.78, 'pressure': 1020, 'humidity': 37},'visibility': 10000, 'wind': {'speed': 4.1, 'deg': 100}, 'clouds': {'all': 39}, 'dt': 1582901422, 'sys': {'type': 1, 'id': 6391, 'country': 'ES', 'sunrise': 1582871792, 'sunset': 1582912379}, 'timezone': 3600, 'id': 2521978, 'name': 'Alicante', 'cod': 200}
-            # r = {'cod': '404', 'message': 'city not found'}
             code = r['cod']
             if r['cod'] == 200:
                 city_weather = {
